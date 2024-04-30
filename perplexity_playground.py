c01205ca02e555a84e39e34b8611e7f032e50113
Saran Chockan <saran.arun@utexas.edu>
2024-04-29 23:00:02 -0400

    refactoring

--- perplexity_playground.py
+++ perplexity_playground.py
@@ -1,9 +1,5 @@
 import requests
 
-COMPANY_NAME = "Benchling"
-EXTRACT_COMPANY_CAREER_PAGE_URL_PROMPT = f"""Can you output the URL that is the 
-official careers page for a tech company called {COMPANY_NAME}? 
-Remember, YOUR OUTPUT SHOULD ONLY BE THE URL and NOTHING ELSE."""
 
 PERPLEXITY_API_TOKEN = "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"
 
@@ -15,17 +11,12 @@ headers = {
 }
 
 
-def call_perpexity_llm(prompt: str) -> str:
+def call_perpexity_llm(sys_prompt: str, user_prompt: str) -> str:
     payload = {
         "model": "sonar-small-online",
         "messages": [
-            {
-                "role": "system",
-                "content": """You are a website crawler. You will be given the name of a company. 
-You should output the URL of the company's career page. 
-Remember, YOUR OUTPUT SHOULD ONLY BE THE URL and NOTHING ELSE""",
-            },
-            {"role": "user", "content": prompt},
+            {"role": "system", "content": sys_prompt},
+            {"role": "user", "content": user_prompt},
         ],
     }
     response = requests.post(url, json=payload, headers=headers).json()
--- playwright_playground.py
+++ playwright_playground.py
@@ -7,57 +7,69 @@ from perplexity_playground import call_perpexity_llm
 
 
 COMPANY_NAME = os.getenv("COMPANY_NAME")
-EXTRACT_COMPANY_CAREER_PAGE_URL_PROMPT = f"""Can you output the URL that is the 
+EXTRACT_COMPANY_CAREER_PAGE_URL_SYS_PROMPT = """You are a website crawler. You will be given the name of a company. 
+You should output the URL of the company's career page. 
+Remember, YOUR OUTPUT SHOULD ONLY BE THE URL and NOTHING ELSE"""
+EXTRACT_COMPANY_CAREER_PAGE_URL_USER_PROMPT = f"""Can you output the URL that is the 
 official careers page for a tech company called {COMPANY_NAME}? 
 Remember, YOUR OUTPUT SHOULD ONLY BE THE URL and NOTHING ELSE."""
 
+SEARCH_FOR_SOFTWARE_ROLES_SYS_PROMPT = """You are an AI Job Hunter Web Agent who is an expert
+in search for software engineer roles on company career websites. 
+
+You will be given {HTML_INPUT_ELEMENTS}. Each input element in {HTML_INPUT_ELEMENTS} is an   
+You should pick one input element that is prompting the user to search for specific job openings in the company. 
+
+HTML_INPUT_ELEMENTS = [
+"<input data-testid="SearchInput" color="pink80" name="search" placeholder="Openings" class="sc-bc6dc228-2 hQcbLS" value="">"
+"<input data-testid="core-ui-dropdown" name="categoryFiter" role="combobox" aria-expanded="false" aria-autocomplete="none" aria-readonly="false" aria-describedby="core-ui-id-8154769791-description" aria-invalid="false" id="core-ui-id-8154769791" readonly="" class="sc-1v6kknp-5 hsPasW">"
+"<input data-testid="core-ui-dropdown" name="locationFilter" role="combobox" aria-expanded="false" aria-autocomplete="none" aria-readonly="false" aria-describedby="core-ui-id-4236106179-description" aria-invalid="false" id="core-ui-id-4236106179" readonly="" class="sc-1v6kknp-5 hsPasW">"
+]
+
+Pick an input element from HTML_INPUT_ELEMENTS. Remember, YOUR OUTPUT SHOULD ONLY BE THE INPUT ELEMENT and NOTHING ELSE.
+
+"""
+
 
 def run(playwright: Playwright):
-
-    print(f"### Extracting software engineering positions from {COMPANY_NAME} ###")
 
     chromium = playwright.chromium
     browser = chromium.launch(headless=False)
     page = browser.new_page()
 
-    # TODO: add error handling
-    company_career_page_url = call_perpexity_llm(EXTRACT_COMPANY_CAREER_PAGE_URL_PROMPT)
+    print(
+        f"✨✨✨ Extracting software engineering positions from {COMPANY_NAME} ✨✨✨"
+    )
+    """
+    ASK perplexity for company's official career page url.
+    """
 
-    print(company_career_page_url)
+    # TODO: Add error handling
+    # TODO: LLM DB Cache: if perplexity has previously
+    # give us this company's career page URL, extract
+    # URL from DB
+    company_career_page_url = call_perpexity_llm(
+        EXTRACT_COMPANY_CAREER_PAGE_URL_SYS_PROMPT,
+        EXTRACT_COMPANY_CAREER_PAGE_URL_USER_PROMPT,
+    )
+
+    print(f"{COMPANY_NAME} Career Page URL: {company_career_page_url}")
 
     page.goto(company_career_page_url)
 
-    input("Press Enter to close the browser...")
+    """
+    SEARCH for software engineer roles in the 
+    company's career page.
+    """
+    page.wait_for_selector("input", timeout=10000)
 
+    input_elements = page.query_selector_all("input")
+    for input_element in input_elements:
+        print(input_element.get_property("outerHTML"))
+
+    input("Press Enter to close the browser...")
     browser.close()
 
 
 with sync_playwright() as playwright:
     run(playwright)
-
-
-"""
-    Navigate to Careers page from an company's official 
-    website.
-
-    ```
-    page = browser.new_page()
-    page.goto("https://www.notion.so/careers")
-    hyperlink_titles = get_page_hyperlink_titles(page=page)
-
-    print("Navigating to Notion Careers...")
-    page.get_by_text("Careers").click()
-    ```
-"""
-
-
-def get_page_hyperlink_titles(page: Page) -> List[str]:
-    ret = []
-    # Get all the hyperlinks on the page
-    links = page.query_selector_all("a")
-    # Print the title of each link
-    for link in links:
-        title = link.get_attribute("title")
-        title = title if title else link.text_content()
-        ret.append(title)
-    return ret
